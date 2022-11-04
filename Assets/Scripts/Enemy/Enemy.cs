7159734c7c3801b31ee999bcdec7fdd3611ddbac
LiubovPil <alexandraelle95@gmail.com>
2022-11-04 21:37:41 +0300

    Adding movement to enemy

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,17 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 [RequireComponent(typeof(HealthSystem))]
 public class Enemy : MonoBehaviour, ITarget
 {
-    private HealthSystem _healthSystem;
+    [Header("Settings for movement")]
+    [SerializeField] private GameObject _player;
+
+    [Header("Settings for movement")]
+    [SerializeField] private float _maxDistanceToPlayer = 1.5f;
+    [SerializeField] private int _enemySpeed = 100;
+
+    private HealthSystem _enemyHealthSystem;
+    private NavMeshAgent _enemyMeshAgent;
 
     private void Awake()
     {
-        _healthSystem = GetComponent<HealthSystem>();
+        _enemyHealthSystem = GetComponent<HealthSystem>();
+        _enemyMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    private void Start()
+    {
+        if (_enemyMeshAgent != null)
+            _enemyMeshAgent.SetDestination(_player.transform.position);
+    }
+    private void Update()
+    {
+        if(_enemyMeshAgent != null)
+            CheckDistance();
+    }
+    private void CheckDistance()
+    {
+        float distance = Vector3.Distance(_player.transform.position, this.transform.position);
+
+        if (distance <= _maxDistanceToPlayer)
+        {
+            _enemyMeshAgent.isStopped = true;
+        }
+        else
+        {
+            _enemyMeshAgent.isStopped = false;
+            _enemyMeshAgent.SetDestination(_player.transform.position);
+        }
+            
     }
     public void ApplyDamage()
     {
-        _healthSystem.Death();
+        _enemyHealthSystem.Death();
     }
 }
