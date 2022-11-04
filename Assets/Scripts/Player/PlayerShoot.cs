0c1ccbe049a4a27c17d755e5c2643009a4c224c9
LiubovPil <alexandraelle95@gmail.com>
2022-11-04 18:32:50 +0300

    Repair animation

--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -10,16 +10,12 @@ public class PlayerShoot : MonoBehaviour
     [SerializeField] private int _shootDistance = 100;
     [SerializeField] private int _countOfShoot = 7;
     [SerializeField] private int _currentCountOfShoot = 0;
-    
-    [Header("Control of shooting")]
-    [SerializeField] private bool _isShooting = false;
-    [SerializeField] private bool _isReloading = false;
 
     [Header("Camera")]
     [SerializeField] private Camera _mainCamera;
 
     private PlayerActions _playerActions;
-    private Animator _playerAnimator;
+    private AnimationHand _handAnimator;
 
     private void OnEnable()
     {
@@ -31,15 +27,15 @@ public class PlayerShoot : MonoBehaviour
         _playerActions.Actions.Reload.Enable();
         _playerActions.Actions.Reload.performed += context => Reload();
 
-        _playerAnimator = GetComponent<Animator>();
+        _handAnimator = GetComponentInChildren<AnimationHand>();
     }
 
     private void Shoot()
     {
-        if (_currentCountOfShoot < _countOfShoot && !_isShooting && !_isReloading)
+        if (_currentCountOfShoot < _countOfShoot 
+            && _handAnimator.CanShoot)
         {
-            _playerAnimator.SetTrigger("StartShoot");
-            _isShooting = true;
+            _handAnimator.PlayAnim("StartShoot");
 
             RaycastHit hitInformation;
 
@@ -57,14 +53,8 @@ public class PlayerShoot : MonoBehaviour
     }
     private void Reload()
     {
-        _isReloading = true;
-        _playerAnimator.SetTrigger("Reload");
+        _handAnimator.PlayAnim("Reload");
         _currentCountOfShoot = 0;
-    }
-    private void ResetAnimations() //calling from end of animations
-    {
-        _isReloading = false;
-        _isShooting = false;
     }
     private void OnDisable()
     {
