603ac3cdc6fc66c16157dfc760c93d5a8b5fc366
LiubovPil <alexandraelle95@gmail.com>
2022-11-04 15:24:13 +0300

    Adding animator controller for player and animations

--- Assets/Scripts/New Input System/PlayerActions.cs
+++ Assets/Scripts/New Input System/PlayerActions.cs
@@ -44,6 +44,15 @@ public partial class @PlayerActions : IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": true
+                },
+                {
+                    ""name"": ""Reload"",
+                    ""type"": ""Button"",
+                    ""id"": ""b87a6bb3-264c-4908-a1f9-9a1793f2fbb4"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -79,6 +88,17 @@ public partial class @PlayerActions : IInputActionCollection2, IDisposable
                     ""action"": ""Rotation"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""e1a6da67-ac68-432e-80bb-0edba324a92e"",
+                    ""path"": ""<Keyboard>/r"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Reload"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -89,6 +109,7 @@ public partial class @PlayerActions : IInputActionCollection2, IDisposable
         m_Actions = asset.FindActionMap("Actions", throwIfNotFound: true);
         m_Actions_Shoot = m_Actions.FindAction("Shoot", throwIfNotFound: true);
         m_Actions_Rotation = m_Actions.FindAction("Rotation", throwIfNotFound: true);
+        m_Actions_Reload = m_Actions.FindAction("Reload", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -150,12 +171,14 @@ public partial class @PlayerActions : IInputActionCollection2, IDisposable
     private IActionsActions m_ActionsActionsCallbackInterface;
     private readonly InputAction m_Actions_Shoot;
     private readonly InputAction m_Actions_Rotation;
+    private readonly InputAction m_Actions_Reload;
     public struct ActionsActions
     {
         private @PlayerActions m_Wrapper;
         public ActionsActions(@PlayerActions wrapper) { m_Wrapper = wrapper; }
         public InputAction @Shoot => m_Wrapper.m_Actions_Shoot;
         public InputAction @Rotation => m_Wrapper.m_Actions_Rotation;
+        public InputAction @Reload => m_Wrapper.m_Actions_Reload;
         public InputActionMap Get() { return m_Wrapper.m_Actions; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -171,6 +194,9 @@ public partial class @PlayerActions : IInputActionCollection2, IDisposable
                 @Rotation.started -= m_Wrapper.m_ActionsActionsCallbackInterface.OnRotation;
                 @Rotation.performed -= m_Wrapper.m_ActionsActionsCallbackInterface.OnRotation;
                 @Rotation.canceled -= m_Wrapper.m_ActionsActionsCallbackInterface.OnRotation;
+                @Reload.started -= m_Wrapper.m_ActionsActionsCallbackInterface.OnReload;
+                @Reload.performed -= m_Wrapper.m_ActionsActionsCallbackInterface.OnReload;
+                @Reload.canceled -= m_Wrapper.m_ActionsActionsCallbackInterface.OnReload;
             }
             m_Wrapper.m_ActionsActionsCallbackInterface = instance;
             if (instance != null)
@@ -181,6 +207,9 @@ public partial class @PlayerActions : IInputActionCollection2, IDisposable
                 @Rotation.started += instance.OnRotation;
                 @Rotation.performed += instance.OnRotation;
                 @Rotation.canceled += instance.OnRotation;
+                @Reload.started += instance.OnReload;
+                @Reload.performed += instance.OnReload;
+                @Reload.canceled += instance.OnReload;
             }
         }
     }
@@ -189,5 +218,6 @@ public partial class @PlayerActions : IInputActionCollection2, IDisposable
     {
         void OnShoot(InputAction.CallbackContext context);
         void OnRotation(InputAction.CallbackContext context);
+        void OnReload(InputAction.CallbackContext context);
     }
 }
--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -4,38 +4,72 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
-    [Header("ShootSettings")]
+    [Header("Shoot Settings")]
     [SerializeField] private Vector3 _aimCenter = new Vector3(0.5f, 0.5f, 0f);
     [SerializeField] private Transform _gunEnd;
     [SerializeField] private int _shootDistance = 100;
+    [SerializeField] private int _countOfShoot = 7;
+    [SerializeField] private int _currentCountOfShoot = 0;
+    
+    [Header("Control of shooting")]
+    [SerializeField] private bool _isShooting = false;
+    [SerializeField] private bool _isReloading = false;
 
     [Header("Camera")]
     [SerializeField] private Camera _mainCamera;
 
     private PlayerActions _playerActions;
+    private Animator _playerAnimator;
+
     private void OnEnable()
     {
         _playerActions = new PlayerActions();
+
         _playerActions.Actions.Shoot.Enable();
         _playerActions.Actions.Shoot.performed += context => Shoot();
+
+        _playerActions.Actions.Reload.Enable();
+        _playerActions.Actions.Reload.performed += context => Reload();
+
+        _playerAnimator = GetComponent<Animator>();
     }
 
     private void Shoot()
     {
-        Debug.Log("Shoot!");
-        RaycastHit hitInformation;
-
-        Vector3 rayOrigin = _mainCamera.ViewportToWorldPoint(_aimCenter);
-
-        if (Physics.Raycast(rayOrigin, _mainCamera.transform.forward, out hitInformation, _shootDistance))
+        if (_currentCountOfShoot < _countOfShoot && !_isShooting && !_isReloading)
         {
-            ITarget target = hitInformation.transform.GetComponent<ITarget>();
-            if(target != null)
-                target.ApplyDamage();
+            _playerAnimator.SetTrigger("StartShoot");
+            _isShooting = true;
+
+            RaycastHit hitInformation;
+
+            Vector3 rayOrigin = _mainCamera.ViewportToWorldPoint(_aimCenter);
+
+            if (Physics.Raycast(rayOrigin, _mainCamera.transform.forward, out hitInformation, _shootDistance))
+            {
+                ITarget target = hitInformation.transform.GetComponent<ITarget>();
+                if (target != null)
+                    target.ApplyDamage();
+            }
+
+            _currentCountOfShoot++;
         }
+    }
+    private void Reload()
+    {
+        _isReloading = true;
+        _playerAnimator.SetTrigger("Reload");
+        _currentCountOfShoot = 0;
+    }
+    private void ResetAnimations() //calling from end of animations
+    {
+        _isReloading = false;
+        _isShooting = false;
     }
     private void OnDisable()
     {
         _playerActions.Actions.Shoot.Disable();
+        _playerActions.Actions.Reload.Disable();
     }
+
 }
