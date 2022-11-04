b367c9c0aacdcf7739a91740f2095e07402d6a8e
LiubovPil <alexandraelle95@gmail.com>
2022-11-04 19:48:17 +0300

    Removing material, adding rigidbody to main player

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,25 +4,14 @@ using UnityEngine;
 [RequireComponent(typeof(HealthSystem))]
 public class Enemy : MonoBehaviour, ITarget
 {
-    [Header("Materials of Enemy")]
-    [SerializeField] private Material _enemyDeadMaterial;
-    [SerializeField] private Material _enemyDefaultMaterial;
-
-    private MeshRenderer _enemyRender;
     private HealthSystem _healthSystem;
 
     private void Awake()
     {
-        _enemyRender = GetComponent<MeshRenderer>();
         _healthSystem = GetComponent<HealthSystem>();
-
-        if (_enemyRender != null)
-            _enemyRender.material = _enemyDefaultMaterial;
     }
     public void ApplyDamage()
     {
         _healthSystem.Death();
-        if (_enemyRender != null)
-            _enemyRender.material = _enemyDeadMaterial;
     }
 }
