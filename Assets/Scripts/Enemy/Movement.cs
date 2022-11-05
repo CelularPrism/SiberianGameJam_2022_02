6e27274af32d0ca772fb4968c4791a4df4bf1e99
CelularPrism <46044765+CelularPrism@users.noreply.github.com>
2022-11-05 21:21:24 +0700

    Add NavMeshSystem

--- Assets/Scripts/Enemy/Movement.cs
+++ Assets/Scripts/Enemy/Movement.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Enemies
 {
+    [RequireComponent(typeof(NavMeshAgent))]
     public class Movement : MonoBehaviour
     {
+        [SerializeField] private Transform player;
+        private NavMeshAgent _navMesh;
+
+        private void Start()
+        {
+            _navMesh = GetComponent<NavMeshAgent>();
+        }
+
         void Update()
         {
-
+            _navMesh.SetDestination(player.position);
         }
     }
 }
